4bf9b2208f8d4ffb8cb70e65bfe2abb718b25bd5
Barley-okaoka <okadabaku@gmail.com>
2024-07-04 23:06:30 +0900

    HoT: initial revision

---
+++ coin.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int i;
+    int heads=0;
+    printf("Tossing a coin...\n");
+    for(i=0;i<3;i++){
+        printf("Round %d: ",i+1);
+        srand( (int)time(NULL) );
+        if(rand()%2){
+            heads++;
+            printf("Heads\n");
+        }
+        else printf("Tails\n");
+        sleep(1);
+    }
+    printf("Heads: %d, Tails: %d\n",heads,3-heads);
+
+    return 0;
+}
